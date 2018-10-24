c780aff0e38eb71599ae1d44bacd3527bcc8f5d0
Eric Romrell <eric_romrell@byu.edu>
2018-10-23 18:09:03 -0600

    Added hostname

--- test/zmq_client.py
+++ test/zmq_client.py
@@ -4,7 +4,7 @@ context = zmq.Context()
 socket = context.socket(zmq.REQ)
 
 print("Connecting to server...")
-socket.connect("tcp://localhost:1111")
+socket.connect("tcp://Erics-Macbook:1111")
 
 print("Sending request...")
 socket.send("Hello World".encode("utf-8"))
